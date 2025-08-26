aaf8d04ca942d0aa087e49395725d6bff3af9c80
Daniel Angelo Tobias <tobias.danielangelo@gmail.com>
2025-08-26 09:52:56 +0800

    Added default Units

--- server/product/signals.py
+++ server/product/signals.py
@@ -1,6 +1,6 @@
 from django.db.models.signals import post_migrate
 from django.dispatch import receiver
-from .models import Address, Location
+from .models import Address, Location, Unit
 
 
 @receiver(post_migrate)
@@ -16,6 +16,10 @@ def create_default_accounts(sender, **kwargs):
     defaults = [
         (Location, -1, {"address": Address.objects.get(pk=-1), "shelf": "Shelf"}),
         (Location, -2, {"address": Address.objects.get(pk=-1), "shelf": "Storage"}),
+        (Unit, -1, {"name": "pcs"}),
+        (Unit, -2, {"name": "set"}),
+        (Unit, -3, {"name": "ft"}),
+        (Unit, -4, {"name": "m"}),
     ]
 
     for model, id, fields in defaults:
