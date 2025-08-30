62165b1c0e020300f0e678624f8bd47ee1c989e6
Daniel Angelo Tobias <tobias.danielangelo@gmail.com>
2025-08-30 08:49:12 +0800

    Unused imports

--- web/src/components/core/MainView.tsx
+++ web/src/components/core/MainView.tsx
@@ -2,14 +2,11 @@ import { observer } from "mobx-react-lite";
 import { useEffect } from "react";
 import { Route, Routes, useNavigate } from "react-router-dom";
 import Chat from "../../blueprints/ChatRoom";
-import { buildRoutes } from "../../constants/helpers";
-import { Main } from "./_AllComponents";
-import { LandingView } from "./LandingView";
-import { NavBar } from "./NavigationBar";
-import { useStore } from "./Store";
-import { PriceView } from "../product/MatrixPriceComponents";
 import { CompatibilityView } from "../product/CompatibilityComponents";
 import { LabelView } from "../product/LabelComponents";
+import { PriceView } from "../product/MatrixPriceComponents";
+import { NavBar } from "./NavigationBar";
+import { useStore } from "./Store";
 
 export const MainView = observer(() => {
   const navigate = useNavigate();
@@ -26,7 +23,7 @@ export const MainView = observer(() => {
     fetchAll();
   }, []);
 
-  const routes = buildRoutes(Main);
+  // const routes = buildRoutes(Main);
 
   useEffect(() => {
     if (settingStore.theme() === "dark") {
